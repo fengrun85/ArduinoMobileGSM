be8d4c5f7da93d9209394a1fe029201415d101d8
fengrun85 <fengrun@gmail.com>
2018-11-15 00:44:10 +0800

    First Commit : M1 APN Secrets and SG NTP Server

---
+++ arduino_secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_PINNUMBER     ""
+#define SECRET_GPRS_APN      "sunsurf" // replace your GPRS APN
+#define SECRET_GPRS_LOGIN    "65"    // replace with your GPRS login
+#define SECRET_GPRS_PASSWORD "user123" // replace with your GPRS password
