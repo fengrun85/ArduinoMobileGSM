e53090661deb5552cd6315f691d4404aebe885de
fengrun85 <fengrun@gmail.com>
2018-11-17 05:22:20 +0800

    Remove Serial wait for USB port in Setup, Added SEEEDOLED, Removed NTP UDP, Use GSM getTime with RTC instead

--- GSMClient_SG_M1.ino
+++ GSMClient_SG_M1.ino
@@ -20,6 +20,9 @@
 */
 
 #include <MKRGSM.h>
+#include <Wire.h>
+#include <SeeedOLED.h>
+#include <RTCZero.h>
 
 #include "arduino_secrets.h"
 // Please enter your sensitive data in the Secret tab or arduino_secrets.h
@@ -30,13 +33,7 @@ const char GPRS_APN[]      = SECRET_GPRS_APN;
 const char GPRS_LOGIN[]    = SECRET_GPRS_LOGIN;
 const char GPRS_PASSWORD[] = SECRET_GPRS_PASSWORD;
 
-unsigned int localPort = 2390;      // local port to listen for UDP packets
 
-IPAddress timeServer(178, 128, 126, 226); // sg.pool.ntp.org NTP server
-
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 
 // initialize the library instance
 GSMClient client;
@@ -46,13 +43,30 @@ GSM gsmAccess;
 // A UDP instance to let us send and receive packets over UDP
 GSMUDP Udp;
 
+//For RTC Time
+RTCZero rtc;
+unsigned long epoch = 0;
+const byte timeZone = 8; // Setting for GMT offset in hours
+const unsigned short SECS_PER_HOUR = 3600;
+unsigned long localSgTime = 0;
+
+
 void setup()
 {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
+  /* while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
-  }
+  } */
+
+  Wire.begin();
+  SeeedOled.init();  //initialze SEEED OLED display
+
+  SeeedOled.clearDisplay();          //clear the screen and set start position to top left corner
+  SeeedOled.setNormalDisplay();      //Set display to normal mode (i.e non-inverse mode)
+  SeeedOled.setPageMode();           //Set addressing mode to Page Mode
+  SeeedOled.setTextXY(0,0);          //Set the cursor to Xth Page, Yth Column  
+  SeeedOled.putString("Screen init"); //Print the String
 
   Serial.println("Starting Arduino GPRS NTP client.");
   // connection state
@@ -71,88 +85,46 @@ void setup()
   }
 
   Serial.println("\nStarting connection to server...");
-  Udp.begin(localPort);
+
+  epoch = gsmAccess.getTime();
+  Serial.print("GSM Epoch : ");
+  Serial.println(epoch);
+  
+  localSgTime = epoch + (timeZone * SECS_PER_HOUR);
+
+  rtc.begin(); // initialize RTC
+  rtc.setEpoch(localSgTime); // Setting epoch to SG Time
 }
 
 void loop()
 {
-  sendNTPpacket(timeServer); // send an NTP packet to a time server
-  // wait to see if a reply is available
-  delay(1000);
-  if ( Udp.parsePacket() ) {
-    Serial.println("packet received");
-    // We've received a packet, read the data from it
-    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+ 
+  Serial.print(rtc.getDay());
+  Serial.print("/");
+  Serial.print(rtc.getMonth());
+  Serial.print("/");
+  Serial.print(rtc.getYear());
+  Serial.print("\t");
 
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
+  // ...and time
+  print2digits(rtc.getHours());
+  Serial.print(":");
+  print2digits(rtc.getMinutes());
+  Serial.print(":");
+  print2digits(rtc.getSeconds());
 
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = " );
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    const unsigned short eightHours = 28800;
-    // subtract seventy years:
-    unsigned long epoch = (secsSince1900 - seventyYears) + eightHours; //Added by Fengrun for SG +8 GMT
-    // print Unix time:
-    Serial.println(epoch);
+  Serial.println();
 
 
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
-  }
+  SeeedOled.putString("Hello World!"); //Print the String
   // wait ten seconds before asking for the time again
-  delay(20000);
+  delay(30000);
 }
 
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  //Serial.println("1");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  //Serial.println("2");
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
 
-  //Serial.println("3");
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  //Serial.println("4");
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  //Serial.println("5");
-  Udp.endPacket();
-  //Serial.println("6");
+void print2digits(int number) {
+  if (number < 10) {
+    Serial.print("0");
+  }
+  Serial.print(number);
 }
